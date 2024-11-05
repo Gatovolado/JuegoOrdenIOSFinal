fd90b2677b95e481354afc86c405adab6d9344f1
Carlos <carlos_sanchez_m-tsapp1ma2324@cev.com>
2024-11-05 12:43:08 +0100

    copiado el codigo

--- JuegoOrdenIOSFinal/ViewController.swift
+++ JuegoOrdenIOSFinal/ViewController.swift
@@ -1,19 +1,39 @@
 //
 //  ViewController.swift
-//  JuegoOrdenIOSFinal
+//  JuegoTareaIOS
 //
-//  Created by Alumnos on 5/11/24.
+//  Created by user234561 on 15/10/24.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+    
 
+    @IBOutlet weak var imagenCambiante: UIImageView!-
+    var imagenes = [UIImage(named:"RainbowDash"),UIImage(named:"Applejack"),UIImage(named:"Fluttershy"),UIImage(named:"PinkiePie"),UIImage(named:"Rarity"),UIImage(named:"TwilightSparkle")]
+        
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        var imagenesCopia = Array(imagenes)
+        var imagenesPartida = [UIImage]()
+        for _ in imagenes{
+            let numRandom = Int.random(in: 0..<(imagenesCopia.count))
+            imagenesPartida.append(imagenesCopia[numRandom]!)
+            imagenesCopia.remove(at: numRandom)
+        }
+        var indexImagenes = 0
+        
+        Timer.scheduledTimer(withTimeInterval: 2, repeats: true) { (timer) in
+            self.imagenCambiante.image = imagenesPartida[indexImagenes]
+            indexImagenes = indexImagenes + 1
+            print(indexImagenes)
+            if indexImagenes == imagenesPartida.count{
+                timer.invalidate()
+            }
+        }
     }
-
-
+    
+    
 }
-
+    
