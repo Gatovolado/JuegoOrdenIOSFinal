52376eec2db08cb5cdcc50a379fa89637596cdd5
Carlos <carlos_sanchez_m-tsapp1ma2324@cev.com>
2024-11-13 10:36:46 +0100

    la aplicacion hace cosas pero no tiene puntuacion

--- JuegoOrdenIOSFinal/ViewController.swift
+++ JuegoOrdenIOSFinal/ViewController.swift
@@ -6,39 +6,44 @@
 //
 
 import UIKit
+var imagenesPartida = [UIImage]()
+var imagenes = [UIImage(named:"RainbowDash"),UIImage(named:"Applejack"),UIImage(named:"Fluttershy"),UIImage(named:"PinkiePie"),UIImage(named:"Rarity"),UIImage(named:"TwilightSparkle")]
+var indice = [0,1,3,4,5,2]
+var indicePartida = [Int]()
 
 class ViewController: UIViewController {
     
-
-    
     @IBOutlet weak var imagenCambiante: UIImageView!
     @IBOutlet weak var botonEmpezarJuego: UIButton!
-    var imagenes = [UIImage(named:"RainbowDash"),UIImage(named:"Applejack"),UIImage(named:"Fluttershy"),UIImage(named:"PinkiePie"),UIImage(named:"Rarity"),UIImage(named:"TwilightSparkle")]
+    
+    var imagenesPartida2 = [UIImage]()
         
     override func viewDidLoad() {
+        
         self.imagenCambiante.alpha = 1
         self.botonEmpezarJuego.isHidden = true
         var imagenesCopia = Array(imagenes)
-        var imagenesPartida = [UIImage]()
+        
         for _ in imagenes{
             let numRandom = Int.random(in: 0..<(imagenesCopia.count))
             imagenesPartida.append(imagenesCopia[numRandom]!)
+            print(imagenesPartida)
+            indicePartida.append(indice[numRandom])
             imagenesCopia.remove(at: numRandom)
         }
+        imagenesPartida2 = Array(imagenesPartida)
         var indexImagenes = 0
         
-        Timer.scheduledTimer(withTimeInterval: 2, repeats: true) { (timer) in
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { (timer) in
             self.imagenCambiante.image = imagenesPartida[indexImagenes]
             indexImagenes = indexImagenes + 1
-            print(indexImagenes)
+            //print(indexImagenes)
             if indexImagenes == imagenesPartida.count{
                 timer.invalidate()
                 self.botonEmpezarJuego.isHidden = false
             }
         }
-        
     }
-    
     
 }
     
--- JuegoOrdenIOSFinal/pantallaBotones.swift
+++ JuegoOrdenIOSFinal/pantallaBotones.swift
@@ -2,7 +2,43 @@
 //  pantallaBotones.swift
 //  JuegoOrdenIOSFinal
 //
-//  Created by Alumnos on 11/11/24.
+//  Created by Alumnos on 12/11/24.
 //
 
-import Foundation
+import UIKit
+
+class pantallaBotones: UIViewController{
+    var puntuacion = 0
+    var indiceUsuario = [Int]()
+    
+    @IBOutlet weak var rainbowButton: UIButton!
+    @IBOutlet weak var AppleJackButton: UIButton!
+    @IBOutlet weak var TwilightButton: UIButton!
+    @IBOutlet weak var FluttershyButton: UIButton!
+    @IBOutlet weak var PinkieButton: UIButton!
+    @IBOutlet weak var RarityButton: UIButton!
+    
+    @IBOutlet weak var puntuacionText: UILabel!
+    
+    @IBAction func Botones(_ sender: UIButton) {
+        
+        indiceUsuario.append(sender.tag)
+        print("tag: " + String(sender.tag))
+        
+    }
+    override func viewDidLoad(){
+        for x in indicePartida{
+            print("x: " + String(x))
+            for y in indiceUsuario{
+                print("y: " + String(y))
+                if x == y{
+                    puntuacion += 1
+                    print("punt: " + String(puntuacion))
+                }
+            }
+        }
+        
+        puntuacionText.text = "Puntuacion total " + String(puntuacion)
+    }
+    
+}
