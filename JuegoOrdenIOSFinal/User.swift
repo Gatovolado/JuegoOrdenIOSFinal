244d444dbf7fdda75661a87178200746f6e546fd
Carlos <carlos_sanchez_m-tsapp1ma2324@cev.com>
2024-11-19 11:42:34 +0100

    creada la clase usuario

---
+++ JuegoOrdenIOSFinal/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  JuegoOrdenIOSFinal
+//
+//  Created by Alumnos on 19/11/24.
+//
+
+import Foundation
+
+class Usuario : Decodable {
+    var user: String
+    var pass: String
+}
